7279a1a2f115488563d8da76bb692ca8fa501695
Iryna Bolbat <irinabolbat98@gmail.com>
2024-11-29 21:52:49 +0200

    add generateStaticParams to cart

--- src/app/cart/[id]/page.tsx
+++ src/app/cart/[id]/page.tsx
@@ -1,10 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCollectionAsync } from "@/store/slices/collectionSlice";
-import { AppDispatch, RootState } from "@/store/store";
-
 import Loader from "@/app/components/Loader";
 import ProductPage from "@/app/components/Product";
 import { Dress } from "@/types/Dress";
@@ -16,33 +9,19 @@ type ProductProps = {
   };
 };
 
-export async function generateStaticParams() {
-  // Пример: данные можно взять из API, базы данных или определить вручную
-  const catalogItems = [
-    { id: '1' },
-    { id: '2' },
-    { id: '3' },
-  ];
-
-  // Возвращаем массив параметров
-  return catalogItems.map((item) => ({
-    id: item.id,
-  }));
-}
-
-
-
-export default function Product({ params: { id } }: ProductProps) {
-  const dispatch = useDispatch<AppDispatch>();
-  const collection = useSelector((state: RootState) => state.collection.value);
-
-  useEffect(() => {
-    dispatch(fetchCollectionAsync());
-  }, [dispatch]);
+export default async function Product({ params: { id } }: ProductProps) {
+  const collection: Dress[] = await fetchCollection()
 
   const product = collection.find((dress: Dress) => dress.id === id) || null;
 
   if (!product) return <Loader />;
 
   return <ProductPage product={product} />;
+}
+
+export async function generateStaticParams() {
+  const collection: Dress[] = await fetchCollection();
+  return collection.map((item: Dress) => ({
+    id: item.id,
+  }));
 }
